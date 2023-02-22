084abda613fcf67debe18838cf78fcce4192cfa8
Faisal <mfaisalhidayatullah@gmail.com>
2023-02-22 12:36:51 +0700

    feat: create repository

---
+++ repository/product_repository_impl.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"mfaisalh12/product-restful-api/helper"
+	"mfaisalh12/product-restful-api/model/domain"
+)
+
+type ProductRepositoryImpl struct {
+}
+
+
+func (repository *ProductRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, product domain.Product) domain.Product {
+	SQL := "INSERT INTO products(name, description, category, price) VALUES(?, ?, ?, ?)"
+	result, err := tx.ExecContext(ctx, SQL, product.Name, product.Description, product.Category, product.Price)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	product.Id = int(id)
+	return product
+}
+
+func (repository *ProductRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, product domain.Product) domain.Product {
+	SQL := "UPDATE products SET name = ?, description = ?, category = ?, price = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, product.Name, product.Description, product.Category, product.Price, product.Id)
+	helper.PanicIfError(err)
+
+	return product
+}
+
+func (repository *ProductRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, product domain.Product) {
+	SQL := "DELETE FROM products WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, product.Id)
+	helper.PanicIfError(err)
+}
+
+func (repository *ProductRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, productId int) (domain.Product, error) {
+	SQL := "SELECT * FROM products WHERE id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, productId)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	product := domain.Product{}
+	if rows.Next() {
+		err := rows.Scan(&product.Id, &product.Name, &product.Description, &product.Category, &product.Price)
+		helper.PanicIfError(err)
+		return product, nil
+	} else {
+		return product, errors.New("Product is not found")
+	}
+}
+
+func (repository *ProductRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Product {
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	var products []domain.Product
+	for rows.Next() {
+		product := domain.Product{}
+		err := rows.Scan(&product.Id, &product.Name, &product.Description, &product.Category, &product.Price)
+		helper.PanicIfError(err)
+		products = append(products, product)
+	}
+	return products
+}
